54e9dbad2e3a7aacbb770599905205269215c063
arjun-saseendran <arjunsaseendran@icloud.com>
2025-01-13 22:18:23 +0530

    add customArray.js

---
+++ customArray.js
@@ -0,0 +1,56 @@
+class CustomArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  shift() {
+    const firstItem = this.data[0];
+    for (let i = 0; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+
+    delete this.data[this.length - 1];
+    this.length--;
+    return firstItem;
+  }
+
+  delete(index) {
+    const item = this.data[index];
+
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+    return item;
+  }
+}
+
+const myArray = new CustomArray();
+
+myArray.push("Apple");
+myArray.push("Mango");
+myArray.push("Grapes");
+myArray.push("Kiwi");
+myArray.push("Orange");
+
+myArray.delete(1);
+
+console.log(myArray);
